ad6e5ba8a0290c0dbd351df7db6cda8293e0fe5d
thi-tr <thi.t2103@gmail.com>
2026-03-30 10:53:25 -0400

    Created game logic functions in js (newGame, updatePage, guessLetter) and included them in html

--- hangman.js
+++ hangman.js
@@ -1,19 +1,36 @@
 const POSSIBLE_WORDS = ["obdurate", "verisimilitude", 
     "defenestrate", "obsequious", "dissonant", "toady", "idempotent"];
-
+var word = "";
+var guesses = ""; //this will be all the guesses
 
 let newGame = function(){
     //Pick a random word
     let randomIndex = parseInt(Math.random()*POSSIBLE_WORDS.length);
-    let word = POSSIBLE_WORDS[randomIndex];
-    console.log("Word chosen:", word);
+    word = POSSIBLE_WORDS[randomIndex];
+    guesses="";
+    updatePage();
+}
 
-    //show the dashes
+let updatePage = function(){
     let clueString = "";    
     for(let i = 0; i < word.length; i++){
-        clueString += "_ ";
+        var currentLetter = word.charAt(i);
+        if(guesses.indexOf(currentLetter) >=0){
+            clueString+= currentLetter+" ";
+        }else{
+            clueString += "_ ";
+        }
     }
-
     let clue = document.getElementById("clue");
     clue.textContent = clueString;
+
+    let guessArea = document.getElementById("guesses");
+    guessArea.textContent = "Guesses: " + guesses;
+}
+
+let guessLetter = function(){
+    let input = document.getElementById("guess");
+    let letter = input.value;
+    guesses += letter;
+    updatePage();
 }
