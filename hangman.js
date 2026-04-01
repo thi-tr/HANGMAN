a532752e1ae247225674bc077a0c9f418ebfb168
thi-tr <thi.t2103@gmail.com>
2026-04-01 10:33:32 -0400

    Make image change on wrong guess

--- hangman.js
+++ hangman.js
@@ -2,9 +2,12 @@ const POSSIBLE_WORDS = ["obdurate", "verisimilitude",
     "defenestrate", "obsequious", "dissonant", "toady", "idempotent"];
 var word = "";
 var guesses = ""; //this will be all the guesses
+var guessCount;
+const MAX_GUESSES = 6;
 
 let newGame = function(){
     //Pick a random word
+    guessCount = MAX_GUESSES;
     let randomIndex = parseInt(Math.random()*POSSIBLE_WORDS.length);
     word = POSSIBLE_WORDS[randomIndex];
     guesses="";
@@ -26,11 +29,19 @@ let updatePage = function(){
 
     let guessArea = document.getElementById("guesses");
     guessArea.textContent = "Guesses: " + guesses;
+
+    let image = document.getElementById("hangmanpic");
+    image.src = `images/hangman${guessCount}.gif`
+
 }
 
 let guessLetter = function(){
     let input = document.getElementById("guess");
     let letter = input.value;
+    if(word.indexOf(letter) < 0){
+        guessCount--;
+    }
+    letter = letter.toLowerCase();
     guesses += letter;
     updatePage();
 }
